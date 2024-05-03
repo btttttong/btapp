a2ecc331f0e0393bd055c14c8d714e7dab4c0d08
supakavadee r <tong.7147@gmail.com>
2024-05-03 07:54:24 +0700

    :construction: re-structure of DB

--- database/migrations/2024_04_26_141548_create_category_table.php
+++ database/migrations/2024_04_26_141548_create_category_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('category', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');
+            $table->string('category_name');
         });
     }
 
--- database/migrations/2024_05_01_033225_create_teller_logs_table.php
+++ database/migrations/2024_05_01_033225_create_teller_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('teller_logs', function (Blueprint $table) {
+        Schema::create('teller_log', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignId('day_id');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('teller_logs');
+        Schema::dropIfExists('teller_log');
     }
 };
